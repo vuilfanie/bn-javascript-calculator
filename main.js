abe54a9590c0ab5604b0263123498f9288a610c6
vuilfanie <55847853+vuilfanie@users.noreply.github.com>
2023-02-08 15:03:27 +0200

    feature/javascript
    Added following javascript:
    
    define calc variables using constructor
    
    Parse values passed into the calculator and return float with 15 decimal places.
    
    perform calc
    
    set display function
    
    set calc history function
    
    main calculation process
    
    more notes in main.js

--- main.js
+++ main.js
@@ -75,3 +75,239 @@ function calculate(firstNumber, secondNumber, operator){
     }
     return result
 }
+// code to look at
+
+// // Create and initialize variables
+// class Calculator {
+//     constructor() {
+//       this.curVal = null;
+//       this.secVal = null;
+//       this.dispVal = null;
+//       this.history = [];
+//       this.operator = null;
+//       this.prevOperator = null;
+//       this.reset = false;
+//       this.percentage = false;
+//     }
+  
+//     // Parse values passed into the calculator and return float with 15 decimal places.
+//     parse(val) {
+//       return parseFloat(val.toFixed(15));
+//     }
+  
+//     // Perform calculation
+//     doCalc(op, curVal, secVal, operator = null) {
+//       const calculation = {
+//         "+": (curVal, secVal) => this.parse(curVal + secVal),
+//         "-": (curVal, secVal) => this.parse(curVal - secVal),
+//         x: (curVal, secVal) => this.parse(curVal * secVal),
+//         "÷": (curVal, secVal) => this.parse(curVal / secVal),
+
+//       };
+  
+//       return operator
+//         ? calculation[op](parseFloat(curVal), parseFloat(secVal), operator)
+//         : calculation[op](parseFloat(curVal), parseFloat(secVal));
+//     }
+  
+//     // set display value to the value passed in and then returns the value.
+//     dispatch(val) {
+//       this.dispVal = val;
+//       return val;
+//     }
+  
+//     // set history array to the value passed
+//     dispatchHistory(val) {
+//       return (this.history = val ? [...this.history, val] : []);
+//     }
+  
+//     //updates the history display to the value passed 
+//     //check if value passed is greater than 0, 
+//     // and if it is, it joins all of the values 
+//     //in the array into a string. 
+//     // If it is not, it sets the display to an empty string.
+//     updateHistory(val) {
+//       document.querySelector(".calculator__history").innerHTML =
+//         val.length > 0 ? val.join(" ") : "&nbsp;";
+//     }
+  
+//     // reset calculator to origin
+//     resetCalculator() {
+//       this.curVal = null;
+//       this.secVal = null;
+//       this.operator = null;
+//       this.prevOperator = null;
+//       this.percentage = null;
+//       this.dispVal = null;
+//       this.history = [];
+//       this.reset = false; // Resetting secVal to null
+//     }
+  
+//     // check if value passed is less than or equal to 23
+//     allowInput(val) {
+//       return String(val).length <= 23;
+//     }
+  
+//     // checks if the value passed in contains a period
+//     allowPeriod(val) {
+//       return String(val).indexOf(".") !== -1;
+//     }
+  
+//     // main function "process" 
+//     process(val) {
+//       if (val >= 0 || val <= 9 || val === ".") {
+//         if (!this.curVal && val === ".") {
+//           return 0;
+//         }
+  
+//         if (this.operator === "=") {
+//           this.resetCalculator();
+//         }
+  
+//         // store first input value
+//         if (!this.operator) {
+//           if (this.curVal && !this.allowInput(this.curVal)) {
+//             return this.dispatch(this.curVal);
+//           }
+//           // check if period is allowed
+//           if (val === "." && this.allowPeriod(this.curVal)) {
+//             return this.dispatch(this.curVal);
+//           }
+  
+//           this.curVal ? (this.curVal += val) : (this.curVal = val);
+//           this.dispatch(this.curVal);
+//           // store second input value
+//         } else {
+//           if (this.secVal && !this.allowInput(this.secVal)) {
+//             return this.dispatch(this.secVal);
+//           }
+//           // check if period is allowed
+//           if (val === "." && this.allowPeriod(this.secVal)) {
+//             return this.dispatch(this.secVal);
+//           }
+  
+//           !this.secVal || this.reset ? (this.secVal = val) : (this.secVal += val);
+  
+//           this.dispatch(this.secVal);
+//           // cancel second value reset flag
+//           this.reset = false;
+//         }
+//       } else if (val === "+" || val === "-" || val === "x" || val === "÷") {
+//         // Initiate second value reset
+//         if (this.operator === "=") {
+//           this.operator = val;
+//           this.reset = true;
+//         }
+//         // If both values present, perform calculation
+//         else if (this.curVal && this.secVal) {
+//           this.curVal = this.dispatch(
+//             this.doCalc(this.operator, this.curVal, this.secVal)
+//           );
+//           this.reset = true;
+//         } else if (!this.curVal) {
+//           this.curVal = 0;
+//         }
+  
+//         // history update
+//         if (this.curVal && !this.secVal) {
+//           this.dispatchHistory(this.curVal);
+//           this.dispatchHistory(val);
+//           this.updateHistory(this.history);
+//         } else if (this.secVal) {
+//           this.dispatchHistory(this.secVal);
+//           this.dispatchHistory(val);
+//           this.updateHistory(this.history);
+//         }
+  
+//         this.prevOperator = null;
+//         this.operator = val;
+  
+  
+//         // assignment opertaion
+//       } else if (val === "=") {
+//         if (!this.curVal && !this.secVal) {
+//           return this.dispVal || 0;
+//         }
+  
+//         // store previous operator
+//         this.prevOperator = this.prevOperator ? this.prevOperator : this.operator;
+  
+//         if (this.secVal) {
+          
+//           // Continue calculation with previous operator
+//           // if assignment operator is pressed
+//           if (val === this.operator) {
+//             this.curVal = this.dispatch(
+//               this.doCalc(this.prevOperator, this.curVal, this.secVal)
+//             );
+//           } else {
+//             // perform new calculation and assignment
+//             this.curVal = this.dispatch(
+//               this.doCalc(this.operator, this.curVal, this.secVal)
+//             );
+//             this.operator = val;
+//             this.dispatchHistory(false);
+//             this.updateHistory(this.history);
+//           }
+  
+//           // if second value was not entered, compute with curVal
+//         } else if (!this.secVal && this.operator) {
+//           this.secVal = this.curVal;
+//           this.curVal = this.dispatch(
+//             this.doCalc(this.operator, this.curVal, this.secVal)
+//           );
+//           this.operator = val;
+//         }
+  
+//         // set negative/positive
+//       } else if (val === "+/-") {
+//         if (
+//           (this.curVal && !this.secVal) ||
+//           (this.curVal && this.operator === "=")
+//         ) {
+//           this.curVal =
+//             parseFloat(this.curVal) > 0
+//               ? -Math.abs(this.curVal)
+//               : Math.abs(this.curVal);
+//           this.dispatch(this.curVal);
+//         } else {
+//           this.secVal =
+//             parseFloat(this.secVal) > 0
+//               ? -Math.abs(this.secVal)
+//               : Math.abs(this.secVal);
+//           this.dispatch(this.secVal);
+//         }
+//       } else if (val === "C") {
+//         this.resetCalculator();
+//         this.updateHistory(this.history);
+//       }
+//       console.log(this);
+//       return this.dispVal || 0;
+//     }
+//   }
+  
+//   (function() {
+//     const state = {};
+//     const init = () => {
+//       if (!state.calculator) {
+//         state.calculator = new Calculator();
+//       }
+//     };
+  
+//     init();
+  
+//     const getInput = val => {
+//       updateDisplay(state.calculator.process(val));
+//     };
+  
+//     const updateDisplay = val => {
+//       document.querySelector(".calculator__display").innerText = val;
+//     };
+  
+//     const els = Array.from(document.querySelectorAll(".calculator__btn"));
+//     els.forEach(el =>
+//       el.addEventListener("click", e => {
+//         const val = e.target.innerText;
+//         getInput(val);
+//       })
+//     )
